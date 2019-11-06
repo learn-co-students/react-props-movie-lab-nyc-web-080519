86c544df59cca977ab4d3e76a862a3d3065b9571
github username <github email address>
2019-11-06 13:14:28 -0500

    Done.

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -1,35 +1,66 @@
 import React, { Component } from 'react';
-import zero from '../assets/stars/0-stars.png'
-import one from '../assets/stars/1-stars.png'
-import two from '../assets/stars/2-stars.png'
-import three from '../assets/stars/3-stars.png'
-import four from '../assets/stars/4-stars.png'
-import five from '../assets/stars/5-stars.png'
+import zero from '../assets/stars/0-stars.png';
+import one from '../assets/stars/1-stars.png';
+import two from '../assets/stars/2-stars.png';
+import three from '../assets/stars/3-stars.png';
+import four from '../assets/stars/4-stars.png';
+import five from '../assets/stars/5-stars.png';
 
-const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
+const imgMapper = { 0: zero, 1: one, 2: two, 3: three, 4: four, 5: five };
 
 export default class CardBack extends Component {
-
   generateRatingElement = () => {
-    // Function to generate star rating for given movie rating props
-    if (this.props.rating === null) {
-      return <h4>"No Rating Found"</h4>
-    } else {
-      return <img src={imgMapper[this.props.rating]} alt=""/>
+    if (this.props.IMDBRating !== null) {
+      return <img src={imgMapper[this.props.IMDBRating]} alt="" />;
     }
-    // no implicit returns in javascript so need to remember to return
-  }
+    return <h4>No Rating Found</h4>;
+  };
 
   render() {
-    let genreArray = this.props.genres.join(", ")
     return (
       <div className="card-back">
         <h3 className="title">{this.props.title}</h3>
         <span />
         {this.generateRatingElement()}
         <span />
-        <h5 className="genres">{genreArray}</h5>
+        <h5 className="genres">{this.props.genres.join(', ')}</h5>
       </div>
-    )
+    );
   }
 }
+
+// import React, { Component } from 'react';
+// import zero from '../assets/stars/0-stars.png'
+// import one from '../assets/stars/1-stars.png'
+// import two from '../assets/stars/2-stars.png'
+// import three from '../assets/stars/3-stars.png'
+// import four from '../assets/stars/4-stars.png'
+// import five from '../assets/stars/5-stars.png'
+
+// const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
+
+// export default class CardBack extends Component {
+
+//   generateRatingElement = () => {
+//     // Function to generate star rating for given movie rating props
+//     if (this.props.rating === null) {
+//       return <h4>"No Rating Found"</h4>
+//     } else {
+//       return <img src={imgMapper[this.props.rating]} alt=""/>
+//     }
+//     // no implicit returns in javascript so need to remember to return
+//   }
+
+//   render() {
+//     let genreArray = this.props.genres.join(", ")
+//     return (
+//       <div className="card-back">
+//         <h3 className="title">{this.props.title}</h3>
+//         <span />
+//         {this.generateRatingElement()}
+//         <span />
+//         <h5 className="genres">{genreArray}</h5>
+//       </div>
+//     )
+//   }
+// }
